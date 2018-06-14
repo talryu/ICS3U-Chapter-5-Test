c2e214f34a0a80997b6f208a8ce3e91dce3268ed
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-14 14:46:26 -0400

    Add files via upload

---
+++ Chapter5_Test - Raman/Chapter5_Extra_Practice5/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnOdd = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnOddAverage = New System.Windows.Forms.Button()
+        Me.lblOddAverage = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(67, 154)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(75, 23)
+        Me.btnShow.TabIndex = 0
+        Me.btnShow.Text = "Average"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(64, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "k"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(64, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "k"
+        '
+        'BtnOdd
+        '
+        Me.BtnOdd.Location = New System.Drawing.Point(67, 213)
+        Me.BtnOdd.Name = "BtnOdd"
+        Me.BtnOdd.Size = New System.Drawing.Size(75, 23)
+        Me.BtnOdd.TabIndex = 3
+        Me.BtnOdd.Text = "Odd"
+        Me.BtnOdd.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(64, 262)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "k"
+        '
+        'btnOddAverage
+        '
+        Me.btnOddAverage.Location = New System.Drawing.Point(67, 298)
+        Me.btnOddAverage.Name = "btnOddAverage"
+        Me.btnOddAverage.Size = New System.Drawing.Size(75, 23)
+        Me.btnOddAverage.TabIndex = 5
+        Me.btnOddAverage.Text = "OddAverage"
+        Me.btnOddAverage.UseVisualStyleBackColor = True
+        '
+        'lblOddAverage
+        '
+        Me.lblOddAverage.AutoSize = True
+        Me.lblOddAverage.Location = New System.Drawing.Point(64, 346)
+        Me.lblOddAverage.Name = "lblOddAverage"
+        Me.lblOddAverage.Size = New System.Drawing.Size(13, 13)
+        Me.lblOddAverage.TabIndex = 6
+        Me.lblOddAverage.Text = "k"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(67, 389)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "65% or Above"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(64, 429)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "k"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(64, 509)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "k"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(67, 469)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(88, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "50% to 65%"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(886, 747)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblOddAverage)
+        Me.Controls.Add(Me.btnOddAverage)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.BtnOdd)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnShow)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnShow As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents BtnOdd As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnOddAverage As System.Windows.Forms.Button
+    Friend WithEvents lblOddAverage As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Chapter5_Test - Raman/Chapter5_Extra_Practice5/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+
+    Private Sub btnShow_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnShow.Click
+        Dim count As Integer = 0
+        Dim sum As Integer = 0
+        Dim average As Decimal
+        For i = 2 To 22
+            Label1.Text = Label1.Text & "  " & i
+            sum += i
+            count = count + 1
+            average = sum / count
+        Next
+        Label2.Text = "Average = " & "  " & average
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnOdd.Click
+        Dim count As Integer = 0
+        Dim num As Integer
+
+        Do While count < 10
+            count = count + 1
+            num = InputBox("enter the number")
+            num = num
+            If num Mod 2 <> 0 Then
+                Label3.Text = Label3.Text & "  " & num
+            End If
+
+        Loop
+    End Sub
+
+    Private Sub btnOddAverage_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOddAverage.Click
+        Dim count As Integer = 0
+        Dim num As Integer
+        Dim sum As Integer = 0
+
+        Do While count < 10
+            count = count + 1
+            num = InputBox("enter the number")
+
+
+            If count Mod 2 = 1 Then
+                sum += num
+
+            End If
+        Loop
+        lblOddAverage.Text = lblOddAverage.Text & "  " & sum
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim count As Integer = 0
+        Dim num As Integer
+
+
+        Do While count < 10
+
+            num = InputBox("enter the number")
+
+
+            If num >= 65 Then
+                count += 1
+
+            End If
+        Loop
+        Label4.Text = Label4.Text & "  " & count
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim count As Integer = 0
+        Dim num As Integer
+
+
+        Do While count < 10
+
+            num = InputBox("enter the number")
+
+
+            If num >= 50 And num <= 65 Then
+                count += 1
+
+            End If
+        Loop
+        Label5.Text = Label5.Text & "  " & count
+    End Sub
+End Class
