e064ac4ce76fcabaf45dd40c2d55fa698d571baf
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-14 14:43:20 -0400

    Add files via upload

---
+++ Michael/Question 1/Number Program/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("Please Enter Numbers")
+        Else
+            Label1.Text = ""
+            Label2.Text = ""
+            Dim firstNumber As Integer = TextBox1.Text
+            Dim secondNumber As Integer = TextBox2.Text
+            Dim normalNumber As Integer
+            Dim oldFirstNumber As Integer
+            Dim oldSecondNumber As Integer
+            If firstNumber > secondNumber Then
+                oldFirstNumber = firstNumber
+                oldSecondNumber = secondNumber
+                firstNumber = oldSecondNumber
+                secondNumber = oldFirstNumber
+            End If
+            For i As Integer = firstNumber To secondNumber - 1
+                Label1.Text += firstNumber + i & vbCrLf
+                normalNumber = firstNumber + i
+                Label2.Text += (normalNumber * normalNumber) & vbCrLf
+            Next
+        End If
+    End Sub
+End Class
---
+++ Michael/Question 2/String Concat/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim newWord As String
+        Dim wordLength As Integer
+        Do
+            newWord = InputBox("Enter New Word, Enter period at end of word to stop.", "Enter Words")
+            wordLength = newWord.Length
+            Label1.Text += newWord & Space(1)
+        Loop Until newWord.Chars(wordLength - 1) = "."
+    End Sub
+End Class
