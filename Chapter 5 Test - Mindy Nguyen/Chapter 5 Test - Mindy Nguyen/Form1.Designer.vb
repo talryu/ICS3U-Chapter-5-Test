f5fdd73acc131ba9d003e66d1f553e1ce56af7ad
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-14 14:40:25 -0400

    Add files via upload

---
+++ Chapter 5 Test - Mindy Nguyen/Chapter 5 Test - Mindy Nguyen/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInstruction = New System.Windows.Forms.Label()
+        Me.btnFact = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.txtNum = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblInstruction
+        '
+        Me.lblInstruction.AutoSize = True
+        Me.lblInstruction.Location = New System.Drawing.Point(16, 43)
+        Me.lblInstruction.Name = "lblInstruction"
+        Me.lblInstruction.Size = New System.Drawing.Size(81, 13)
+        Me.lblInstruction.TabIndex = 0
+        Me.lblInstruction.Text = "Enter a Number"
+        '
+        'btnFact
+        '
+        Me.btnFact.Location = New System.Drawing.Point(140, 51)
+        Me.btnFact.Name = "btnFact"
+        Me.btnFact.Size = New System.Drawing.Size(115, 34)
+        Me.btnFact.TabIndex = 1
+        Me.btnFact.Text = "Compute Factorial"
+        Me.btnFact.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(16, 82)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(0, 13)
+        Me.lblOutput.TabIndex = 2
+        '
+        'txtNum
+        '
+        Me.txtNum.Location = New System.Drawing.Point(19, 59)
+        Me.txtNum.Name = "txtNum"
+        Me.txtNum.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(991, 578)
+        Me.Controls.Add(Me.txtNum)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnFact)
+        Me.Controls.Add(Me.lblInstruction)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblInstruction As System.Windows.Forms.Label
+    Friend WithEvents btnFact As System.Windows.Forms.Button
+    Friend WithEvents lblOutput As System.Windows.Forms.Label
+    Friend WithEvents txtNum As System.Windows.Forms.TextBox
+
+End Class
---
+++ Chapter 5 Test - Mindy Nguyen/Chapter 5 Test - Mindy Nguyen/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub btnFact_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFact.Click
+        Dim factorial As Double = 1
+        Dim number As Integer
+
+        number = Val(Me.txtNum.Text)
+        For count As Integer = 1 To number
+            factorial *= count
+
+        Next count
+        Me.lblOutput.Text = factorial
+    End Sub
+End Class
