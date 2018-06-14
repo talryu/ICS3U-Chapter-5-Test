2775b29756877fe784d5707e604f899093f8b4f4
Tommy Choi <40275760+talryu@users.noreply.github.com>
2018-06-14 14:25:58 -0400

    Add files via upload

---
+++ Alec Lin/Chapter 5 Test Part 2/Chapter 5 Test Part 2/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Word As String
+        Word = InputBox("Enter a word to be combined. Enter a period to stop,", "Word Combiner")
+        If Word.IndexOf(".") = "-1" Then
+            Label1.Text = Word
+            Do Until Word.IndexOf(".") <> "-1"
+                Word = InputBox("Enter a word to be combined. Enter a period to stop,", "Word Combiner")
+                Label1.Text += Word & " "
+            Loop
+        Else
+            Label1.Text = Word
+        End If
+    End Sub
+End Class
---
+++ Alec Lin/Chapter 5 Test/Chapter 5 Test/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Start As Integer = TextBox1.Text
+        Dim EndNum As Integer = TextBox2.Text
+        Dim Count As Integer = 0
+        Dim CurNum As Integer = Start
+        If EndNum < Start Then
+            MsgBox("Invalid Input. Enter smaller number first.")
+            TextBox1.Clear()
+            TextBox2.Clear()
+        Else
+            For i = Start To EndNum
+                If Count Mod 3 <> 0 Or Count = 0 Then
+                    Label3.Text += CurNum & " "
+                    Label4.Text += CurNum ^ 2 & " "
+                    Count += 1
+                    CurNum += 1
+                Else
+                    Label3.Text += vbCrLf & CurNum & " "
+                    Label4.Text += vbCrLf & CurNum ^ 2 & " "
+                    Count += 1
+                    CurNum += 1
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        Label3.Text = ""
+        Label4.Text = ""
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        Label3.Text = ""
+        Label4.Text = ""
+    End Sub
+End Class
